01b2ac7a7630c16fc0906899e1dabffe225163fc
NewCJ <>
2025-04-12 06:36:52 +0700

    Create Project

---
+++ src/test/java/api/game_event_notifier/GameEventNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package api.game_event_notifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameEventNotifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
